0a4652c10a24dfadbf95240a4198223613b77a70
Maschell <Maschell@gmx.de>
2020-05-03 14:16:11 +0200

    Add a function to unload all plugins from memory in the PluginManagement

--- source/PluginManagement.h
+++ source/PluginManagement.h
@@ -16,4 +16,6 @@ public:
     static void PatchFunctionsAndCallHooks(plugin_information_t *gPluginInformation);
 
     static bool doRelocation(const std::vector<RelocationData> &relocData, relocation_trampolin_entry_t *tramp_data, uint32_t tramp_length, uint32_t trampolinID);
+
+    static void unloadPlugins(plugin_information_t * pluginInformation, MEMHeapHandle pluginHeap);
 };

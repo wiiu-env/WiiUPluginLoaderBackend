fddd8842f257f84c7ab185c17d8456ecf5fa74c0
Maschell <Maschell@gmx.de>
2020-05-17 20:40:05 +0200

    Move global variables into a own file

---
+++ source/globals.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <plugin/PluginContainer.h>
+#include <common/plugin_defines.h>
+
+extern plugin_information_t *gPluginInformation;
+extern MEMHeapHandle pluginDataHeap;
+extern plugin_information_on_reload_t gLinkOnReload;

8350766c40f072e5ead72e08699baa1667221ff6
Maschell <Maschell@gmx.de>
2020-05-17 20:43:04 +0200

    Add missing copy constructors

--- source/plugin/PluginData.h
+++ source/plugin/PluginData.h
@@ -48,6 +48,8 @@ public:
     void freeMemory();
 
 private:
+    PluginData(const PluginData &obj);
+
     PluginData() {
     }
 
---
+++ source/plugin/PluginMetaInformation.cpp
@@ -0,0 +1,11 @@
+#include "PluginMetaInformation.h"
+
+PluginMetaInformation::PluginMetaInformation(const PluginMetaInformation& other) {
+    this->name = other.name;
+    this->author = other.author;
+    this->version = other.version;
+    this->license = other.license;
+    this->buildtimestamp = other.buildtimestamp;
+    this->description = other.description;
+    this->size = other.size;
+}

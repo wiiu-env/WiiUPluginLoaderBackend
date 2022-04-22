4f70f69cbda0d3eb02e0f75b84acb29d2ccb5656
Maschell <Maschell@gmx.de>
2022-04-22 22:47:41 +0200

    Control verbose logging via buildflags

--- source/utils/logger.h
+++ source/utils/logger.h
@@ -7,12 +7,19 @@
 extern "C" {
 #endif
 
+#define __FILENAME_X__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
+#define __FILENAME__   (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILENAME_X__)
+
 #ifdef DEBUG
 
-#define __FILENAME_X__                            (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
-#define __FILENAME__                              (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILENAME_X__)
-
+#ifdef VERBOSE_DEBUG
+#define DEBUG_FUNCTION_LINE_VERBOSE(FMT, ARGS...)                                                \
+    do {                                                                                         \
+        WHBLogPrintf("[%23s]%30s@L%04d: " FMT "", __FILENAME__, __FUNCTION__, __LINE__, ##ARGS); \
+    } while (0)
+#else
 #define DEBUG_FUNCTION_LINE_VERBOSE(FMT, ARGS...) while (0)
+#endif
 
 #define DEBUG_FUNCTION_LINE(FMT, ARGS...)                                                        \
     do {                                                                                         \

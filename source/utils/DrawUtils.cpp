184ca0bc5caf698771554ae318f47fb0c82e6b35
Thomas Rohloff <v10lator@myway.de>
2023-10-03 11:23:58 +0200

    Don't draw transparent pixels
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- source/utils/DrawUtils.cpp
+++ source/utils/DrawUtils.cpp
@@ -60,6 +60,10 @@ void DrawUtils::clear(Color col) {
 }
 
 void DrawUtils::drawPixel(uint32_t x, uint32_t y, uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
+    if (a == 0) {
+        return;
+    }
+
     float opacity = a / 255.0f;
 
     // put pixel in the drc buffer

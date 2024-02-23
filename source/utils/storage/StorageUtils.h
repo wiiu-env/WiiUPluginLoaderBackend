c38ef605cc9abd1081cb062fe80f7dd7d0475bd1
Maschell <Maschell@gmx.de>
2024-02-23 11:07:56 +0100

    StorageAPI: Update GetItemSize to take an additional itemType parameter to avoid a accidential string to binary conversion (e.g. for hashes)

--- source/utils/storage/StorageUtils.h
+++ source/utils/storage/StorageUtils.h
@@ -17,5 +17,5 @@ namespace StorageUtils::API {
     WUPSStorageError GetSubItem(wups_storage_root_item root, wups_storage_item parent, const char *key, wups_storage_item *outItem);
     WUPSStorageError StoreItem(wups_storage_root_item root, wups_storage_item parent, const char *key, WUPSStorageItemType itemType, void *data, uint32_t length);
     WUPSStorageError GetItem(wups_storage_root_item root, wups_storage_item parent, const char *key, WUPSStorageItemType itemType, void *data, uint32_t maxSize, uint32_t *outSize);
-    WUPSStorageError GetItemSize(wups_storage_root_item root, wups_storage_item parent, const char *key, uint32_t *outSize);
+    WUPSStorageError GetItemSize(wups_storage_root_item root, wups_storage_item parent, const char *key, WUPSStorageItemType itemType, uint32_t *outSize);
 } // namespace StorageUtils::API

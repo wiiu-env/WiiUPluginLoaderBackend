a66bd1f27511a6d98a3c204d0cfb998ea62f0531
Maschell <Maschell@gmx.de>
2024-03-01 11:53:16 +0100

    Simplify NotificationUtils

--- source/NotificationsUtils.h
+++ source/NotificationsUtils.h
@@ -2,10 +2,6 @@
 
 #include <string>
 
-void StartNotificationThread();
-
-void StopNotificationThread();
-
 bool DisplayInfoNotificationMessage(std::string_view text, float duration);
 
 bool DisplayErrorNotificationMessage(std::string_view text, float duration);

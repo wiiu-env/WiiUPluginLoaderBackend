d2adc304f04dfcbc75bff206bb7db2333c75825a
Maschell <Maschell@gmx.de>
2024-03-23 21:12:27 +0100

    Move global variables out of data section

--- source/globals.cpp
+++ source/globals.cpp
@@ -1,15 +1,15 @@
 #include "globals.h"
 
-StoredBuffer gStoredTVBuffer __attribute__((section(".data")))  = {};
-StoredBuffer gStoredDRCBuffer __attribute__((section(".data"))) = {};
+StoredBuffer gStoredTVBuffer  = {};
+StoredBuffer gStoredDRCBuffer = {};
 
-std::vector<std::unique_ptr<PluginContainer>> gLoadedPlugins __attribute__((section(".data")));
-std::vector<relocation_trampoline_entry_t> gTrampData __attribute__((section(".data")));
+std::vector<std::unique_ptr<PluginContainer>> gLoadedPlugins;
+std::vector<relocation_trampoline_entry_t> gTrampData;
 
-std::set<std::shared_ptr<PluginData>> gLoadedData __attribute__((section(".data")));
-std::set<std::shared_ptr<PluginData>> gLoadOnNextLaunch __attribute__((section(".data")));
-std::mutex gLoadedDataMutex __attribute__((section(".data")));
-std::map<std::string, OSDynLoad_Module> gUsedRPLs __attribute__((section(".data")));
-std::vector<void *> gAllocatedAddresses __attribute__((section(".data")));
+std::set<std::shared_ptr<PluginData>> gLoadedData;
+std::set<std::shared_ptr<PluginData>> gLoadOnNextLaunch;
+std::mutex gLoadedDataMutex;
+std::map<std::string, OSDynLoad_Module> gUsedRPLs;
+std::vector<void *> gAllocatedAddresses;
 
-bool gNotificationModuleLoaded __attribute__((section(".data"))) = false;
+bool gNotificationModuleLoaded = false;

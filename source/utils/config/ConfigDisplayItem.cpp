84f9c4fab98a0dab20513ef76210913b68b9bdc7
Maschell <Maschell@gmx.de>
2024-08-04 15:07:30 +0200

    Save info is plugin is linked and loaded inside config info

--- source/utils/config/ConfigDisplayItem.cpp
+++ source/utils/config/ConfigDisplayItem.cpp
@@ -1,12 +1,22 @@
 #include "ConfigDisplayItem.h"
 #include "config/WUPSConfig.h"
 
-ConfigDisplayItem::ConfigDisplayItem(GeneralConfigInformation &info, std::unique_ptr<WUPSConfigAPIBackend::WUPSConfig> config) : mConfig(std::move(config)), mInfo(std::move(info)) {
+ConfigDisplayItem::ConfigDisplayItem(GeneralConfigInformation &info,
+                                     std::unique_ptr<WUPSConfigAPIBackend::WUPSConfig> config,
+                                     const bool isActive) : mConfig(std::move(config)),
+                                                            mInfo(std::move(info)),
+                                                            mIsActivePlugin(isActive) {
     assert(mConfig);
 }
+
 const GeneralConfigInformation &ConfigDisplayItem::getConfigInformation() const {
     return mInfo;
 }
+
 const WUPSConfigAPIBackend::WUPSConfig &ConfigDisplayItem::getConfig() const {
     return *mConfig;
+}
+
+bool ConfigDisplayItem::isActivePlugin() const {
+    return mIsActivePlugin;
 }

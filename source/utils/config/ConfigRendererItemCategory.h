202f7214945eabb388dd805bb96475b4380ec9c8
splatoon1enjoyer <administrator@sdlfoundation.com>
2024-08-08 14:25:18 +0300

    Add controls for viewing off-screen GUI text

--- source/utils/config/ConfigRendererItemCategory.h
+++ source/utils/config/ConfigRendererItemCategory.h
@@ -4,12 +4,12 @@
 
 class ConfigRendererItemCategory : public ConfigRendererItemGeneric {
 public:
-    explicit ConfigRendererItemCategory(const WUPSConfigAPIBackend::WUPSConfigCategory *category) : mCategory(category) {
+    explicit ConfigRendererItemCategory(const WUPSConfigAPIBackend::WUPSConfigCategory *category) : mCategory(category), mTextOffset(0) {
         assert(category);
     }
 
     void Draw(uint32_t yOffset, bool isHighlighted) const override {
-        drawGenericBoxAndText(yOffset, mCategory->getName(), isHighlighted);
+        drawGenericBoxAndText(yOffset, ConfigRenderItemFont::GetOffsettedText(mCategory->getName(), mTextOffset), isHighlighted);
     }
 
     void Update(bool) override {
@@ -22,6 +22,24 @@ public:
     void ResetNeedsRedraw() override {
     }
 
+    void ResetTextOffset() override {
+        mTextOffset = 0;
+    }
+
+    uint32_t GetTextOffset() const override {
+        return mTextOffset;
+    }
+
+    void IncrementTextOffset() override {
+        if (!mCategory) return;
+        ConfigRenderItemFont::IncrementOffset(mCategory->getName(), mTextOffset);
+    }
+
+    void DecrementTextOffset() override {
+        if (mTextOffset > 0) mTextOffset--;
+    }
+
 private:
     const WUPSConfigAPIBackend::WUPSConfigCategory *mCategory;
+    uint32_t mTextOffset = 0;
 };

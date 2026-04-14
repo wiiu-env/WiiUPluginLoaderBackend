b8418a390a9ac6caf169de9b0e7896a342420e11
Maschell <Maschell@gmx.de>
2026-04-14 17:56:02 +0200

    Use non-newlib function to allocate memory for reent structs to make things simpler

--- source/utils/reent.h
+++ source/utils/reent.h
@@ -4,13 +4,9 @@
 
 class PluginContainer;
 
-void *wups_backend_get_context(const void *pluginId, wups_loader_init_reent_errors_t_ *outError);
+bool wups_backend_get_context(const void *pluginId, void **outPtr);
 
-void *wups_backend_set_sentinel();
-
-void wups_backend_restore_head(void *oldHead);
-
-bool wups_backend_register_context(const void *pluginId, void *reentPtr, void (*cleanupFn)(void *), void *oldHead);
+bool wups_backend_register_context(const void *pluginId, void *reentPtr, void (*cleanupFn)(void *));
 
 void ClearReentDataForPlugins(const std::vector<PluginContainer> &plugins);
 

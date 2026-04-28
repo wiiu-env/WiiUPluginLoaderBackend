9b60e2875e82481c12a3bd46ce4297dded8489fd
Maschell <Maschell@gmx.de>
2026-04-28 20:09:43 +0200

    Fix using devoptabs/sockets in `DEINITIALIZE_PLUGIN` hook

--- source/main.cpp
+++ source/main.cpp
@@ -375,7 +375,13 @@ void CleanupPlugins(std::vector<PluginContainer> &pluginsToDeinit) {
         currentThread->reserved[4] = 0;
     }
 
+    CallHook(pluginsToDeinit, WUPS_LOADER_HOOK_INIT_WUT_DEVOPTAB);
+    CallHook(pluginsToDeinit, WUPS_LOADER_HOOK_INIT_WUT_SOCKETS);
+
     CallHook(pluginsToDeinit, WUPS_LOADER_HOOK_DEINIT_PLUGIN);
+
+    CallHook(pluginsToDeinit, WUPS_LOADER_HOOK_FINI_WUT_DEVOPTAB);
+    CallHook(pluginsToDeinit, WUPS_LOADER_HOOK_FINI_WUT_SOCKETS);
     CallHook(pluginsToDeinit, WUPS_LOADER_HOOK_FINI_WRAPPER);
 
     { // legacy reent code
